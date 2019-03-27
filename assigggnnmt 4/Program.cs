c1beb961e659445f8b37ac5dd39000727e595bc2
visualstudio <visualstudio@122-PC35>
2019-03-27 12:07:27 -0400

    11:50 Commit

--- assigggnnmt 4/Program.cs
+++ assigggnnmt 4/Program.cs
@@ -2,38 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace LearningProperties
+namespace LearningAsyncAwayStart
 {
-    class Player
-    {
-        public bool isAlive = true;
-        int health = 100;
-
-        public void Hit()
-        {
-            Random r = new Random();
-            health -= r.Next(5, 50);
-        }
-
-        public void CheckDeath()
-        {
-            if (health <= 0)
-                isAlive = false;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Player player = new Player();
-            for (int i = 0; i < 20; i++)
-            {
-                player.Hit();
-                player.CheckDeath();
-                Console.WriteLine("Is player alive: " + player.isAlive);
-            }
+            Console.WriteLine("downloading file");
+            Download();
+            Console.ReadLine();
+
+        }
+
+        static void Download()
+        {
+            Network.Download((message) => Console.WriteLine("Download complete " + message));
+        }
+    }
+
+    // Imaginary external network library
+
+    class Network
+    {
+        static public void Download(Action<String> callback)
+        {
+            Task.Run(() => {
+                Thread.Sleep(3000);
+                callback("completed");
+            });
         }
     }
 }
