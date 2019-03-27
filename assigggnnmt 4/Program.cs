21f61af4ae1f6a16ca042152a6f275f621f08a90
visualstudio <visualstudio@122-PC35>
2019-03-27 12:06:02 -0400

    11:56 Commit

--- assigggnnmt 4/Program.cs
+++ assigggnnmt 4/Program.cs
@@ -3,28 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Threading;
 
-namespace assigggnnmt_4
+namespace LearningProperties
 {
+    class Player
+    {
+        public bool isAlive = true;
+        int health = 100;
+
+        public void Hit()
+        {
+            Random r = new Random();
+            health -= r.Next(5, 50);
+        }
+
+        public void CheckDeath()
+        {
+            if (health <= 0)
+                isAlive = false;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Download file");
-            Download();
-            Console.ReadLine();
-
-        }
-        static void Download()
-        {
-            Task.Run(() =>
+            Player player = new Player();
+            for (int i = 0; i < 20; i++)
             {
-                Thread.Sleep(3000);
-                Console.WriteLine("Download Complete ");
-            });
+                player.Hit();
+                player.CheckDeath();
+                Console.WriteLine("Is player alive: " + player.isAlive);
+            }
         }
     }
 }
-
-       
